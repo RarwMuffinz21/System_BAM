20e1b6dfae7c4eed19a13d73efb406d89cd78fa3
Nick Smith <portification@gmail.com>
2014-02-13 22:56:25 +0100

    Initial version of client properties

--- client/handshake.cs
+++ client/handshake.cs
@@ -6,6 +6,13 @@ function clientCmdBAM_SendRevision(%revision)
         ServerConnection.bamRevision = mFloor(%revision);
 
         echo("Server is running BAM r" @ ServerConnection.bamRevision);
+        
+        commandToServer('BAM_ClientProp', "steamId", getSteamId());
+        commandToServer('BAM_ClientProp', "downloadMusic", $Pref::Net::DownloadMusic);
+        commandToServer('BAM_ClientProp', "downloadSounds", $Pref::Net::DownloadSounds);
+        commandToServer('BAM_ClientProp', "downloadTextures", $Pref::Net::DownloadTextures);
+        commandToServer('BAM_ClientProp', "defaultFov", $Pref::Player::DefaultFov);
+        commandToServer('BAM_ClientProp', "shaderQuality", $Pref::ShaderQuality);
     }
 }
 
@@ -14,6 +21,38 @@ package BAMC_HandshakePackage
     function GameConnection::setConnectArgs(%a,%b,%c,%d,%e,%f,%g,%h,%i,%j,%k,%l,%m,%n,%o,%p)
     {
         Parent::setConnectArgs(%a,%b,%c,%d,%e,%f,%g,%h,$BAM::Revision,%j,%k,%l,%m,%n,%o,%p);
+    }
+
+    function OptionsDlg::updateFov(%this)
+    {
+        %old = $Pref::Player::DefaultFov;
+        Parent::updateFov(%this);
+
+        if (ServerConnection.bamServer && $Pref::Player::DefaultFov != %old)
+        {
+            if (isEventPending(%this.sendDefaultFov))
+            {
+                cancel(%this.sendDefaultFov);
+            }
+
+            %this.sendDefaultFov = schedule(
+                250, 0, commandToServer,
+                'BAM_ClientProp', "defaultFov",
+                $Pref::Player::DefaultFov
+            );
+        }
+    }
+
+    function OptionsDlg::setShaderQuality(%this, %quality)
+    {
+        %old = $Pref::ShaderQuality;
+        Parent::setShaderQuality(%this, %quality);
+
+        // Not using %quality because it may not be available
+        if (ServerConnection.bamServer && %old != $Pref::ShaderQuality)
+        {
+            commandToServer('BAM_ClientProp', "shaderQuality", $Pref::ShaderQuality);
+        }
     }
 };
 
--- server/handshake.cs
+++ server/handshake.cs
@@ -1,7 +1,27 @@
+function serverCmdBAM_ClientProp(%client, %property, %value)
+{
+    %client.bamProperty[%property] = %value;
+    %client.bamSentProperty[%property] = 1;
+
+    %client.onClientPropChanged(%property, %value);
+}
+
+function GameConnection::setDefaultClientProps(%this)
+{
+    %this.bamProperty["defaultFov"] = 90;
+}
+
+function GameConnection::onClientPropChanged(%this, %property, %value)
+{
+    // Callback for add-ons to package.
+}
+
 package BAMS_HandshakePackage
 {
     function GameConnection::onConnectRequest(%this,%a,%b,%c,%d,%e,%f,%g,%h,%i,%j,%k,%l,%m,%n,%o,%p)
     {
+        %this.setDefaultClientProps();
+
         if (%i !$= "")
         {
             %this.bamClient = 1;
